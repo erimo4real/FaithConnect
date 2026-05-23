0e023a490bfd008053a0165f7d286a44ce692b0b
erimo4real <erimo4real@users.noreply.github.com>
2026-05-23 03:46:40 +0100

    Force IPv4 on pg pool to fix Render->Supabase ENETUNREACH

--- server/src/db.js
+++ server/src/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   max: 10,
   idleTimeoutMillis: 30000,
+  family: 4,
   ...(process.env.DATABASE_SSL === 'true' ? { ssl: { rejectUnauthorized: true } } : {}),
 });
 
